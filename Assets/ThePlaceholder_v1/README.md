87f70f4a30dcdaccd97d28eb225ee8e7593f2a62
CASPERPC\Ghost <caspereally@gmail.com>
2024-10-19 22:01:58 +0200

    my commit after merge

--- Assets/ThePlaceholder_v1/README.md
+++
@@ -1,5 +0,0 @@
-# The Placeholder [![Public Domain (CC0)](https://i.creativecommons.org/p/zero/1.0/80x15.png)](https://creativecommons.org/publicdomain/zero/1.0/)
-
-A simple placeholder for a platformer or RPG.
-
-_This is public domain [under the CC0](https://creativecommons.org/publicdomain/zero/1.0/)._
--- Scripts/lamp.gd
+++ Scripts/lamp.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
-var shrink_rate = 0.1
+@export var shrink_rate = 0.01
 
 func _process(delta):
 	# Gradually decrease the scale over time
-	scale -= Vector2(shrink_rate, shrink_rate) * delta
+	if scale.x > 0.05:
+		scale -= Vector2(shrink_rate, shrink_rate) * delta
+	elif scale.x <= 0.05:
+		#play shatter sound
+		#screen goes black
+		get_tree().change_scene_to_file("res://Scenes/end_screen.tscn")
