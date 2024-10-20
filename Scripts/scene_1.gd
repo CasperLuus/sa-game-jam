75f5f7914f5f55662f274a0776b1e4a81b14b77f
CASPERPC\Ghost <caspereally@gmail.com>
2024-10-20 14:01:46 +0200

    smol fix

--- Scripts/scene_1.gd
+++ Scripts/scene_1.gd
@@ -1,46 +1,48 @@
-extends Area2D
+extends Node2D
 
 @onready var sleep_prompt: RichTextLabel = $"Nest/Sleep Prompt"
+@onready var player_and_light: CharacterBody2D = $"Player And Light"
 
-@export var PLAYER_AND_LIGHT: CharacterBody2D
 @export var GROW_RATE: float = 0.8;
 
-var PLAYER
-var LIGHT
+var player
+var light
 var PLAYER_POINT_LIGHT: PointLight2D
 var LIGHT_POINT_LIGHT: PointLight2D
 
+var IS_IN_CAMP = false
 var SLEEP_ENABLED_BOOL = false
 
 func _on_ready() -> void:
-	PLAYER = PLAYER_AND_LIGHT.get_child(0)
-	LIGHT = PLAYER_AND_LIGHT.get_child(1)
+	player = player_and_light.get_child(0)
+	light = player_and_light.get_child(1)
 	
-	PLAYER_POINT_LIGHT = PLAYER.get_child(3) 
-	LIGHT_POINT_LIGHT = LIGHT.get_child(0)
+	PLAYER_POINT_LIGHT = player.get_child(3) 
+	LIGHT_POINT_LIGHT = light.get_child(0)
 
 func _process(delta: float) -> void:
-	if LIGHT.scale.x < 0.8 and LIGHT_POINT_LIGHT.visible and not PLAYER.FOLLOWING_LIGHT_SHRINK_BOOL:
-		LIGHT.scale += Vector2(1, 1) * GROW_RATE * delta
-	else: 
-		PLAYER.FOLLOWING_LIGHT_SHRINK_BOOL = LIGHT_POINT_LIGHT.visible
+	if light and player:
+		if light.scale.x < 0.8 and LIGHT_POINT_LIGHT.visible and not player.FOLLOWING_LIGHT_SHRINK_BOOL:
+			light.scale += Vector2(1, 1) * GROW_RATE * delta
+		else: 
+			player.FOLLOWING_LIGHT_SHRINK_BOOL = LIGHT_POINT_LIGHT.visible
 
 func _on_camp_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		#PLAYER_POINT_LIGHT.visible = false
-		LIGHT_POINT_LIGHT.visible = false
-		PLAYER.FOLLOWING_LIGHT_SHRINK_BOOL = false
+		IS_IN_CAMP = true
+		if light:
+			LIGHT_POINT_LIGHT.visible = false
+		if player:
+			player.FOLLOWING_LIGHT_SHRINK_BOOL = false
 
 func _on_camp_body_exited(body: Node2D) -> void:
 	# I was trying to make the light start smaller when leaving the cave home, but its not working for some reason.
 	# im going to bed now, this is laters problem
 	print(body.name)
 	if body.name == "Player":
-		LIGHT.scale = Vector2(0.05, 0.05)
-		print(body.FOLLOWING_LIGHT)
-		
-		#PLAYER_POINT_LIGHT.visible = true
-		LIGHT_POINT_LIGHT.visible = true
+		if light:
+			light.scale = Vector2(0.05, 0.05)
+			LIGHT_POINT_LIGHT.visible = true
 
 func _on_nest_body_entered(body: Node2D) -> void:
 	sleep_prompt.visible = true
