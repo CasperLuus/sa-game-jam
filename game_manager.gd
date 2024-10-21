82ef6cd3ac3a311dc7eac2f2c5eb0d4dc6aba7ca
CASPERPC\Ghost <caspereally@gmail.com>
2024-10-21 15:22:40 +0200

    more core memory working tings

--- game_manager.gd
+++ game_manager.gd
@@ -4,6 +4,15 @@ var current_scene = null
 var random = RandomNumberGenerator.new()
 var memory_core_count = 0
 
+var HAS_EYE_MEMORY: bool = false
+var NAME_EYE_MEMORY = "Eye Memory"
+var HAS_HEART_MEMORY: bool = false
+var NAME_HEART_MEMORY = "Heart Memory"
+var HAS_STATUE_MEMORY: bool = false
+var NAME_STATUE_MEMORY = "Statue Memory"
+var HAS_TRAPPED_DOOR_MEMORY: bool = false
+var NAME_TRAPPED_DOOR_MEMORY = "Trapped Door Memory"
+
 func _ready() -> void:
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() -1)
@@ -18,6 +27,27 @@ func _deferred_switch_scene(scene_path):
 	current_scene = s.instantiate()
 	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
+
+func _get_is_enabled_memory_core(name: String) -> bool:
+	if name == NAME_EYE_MEMORY:
+		return !HAS_EYE_MEMORY
+	elif name == NAME_HEART_MEMORY:
+		return !HAS_HEART_MEMORY
+	elif name == NAME_STATUE_MEMORY:
+		return !HAS_STATUE_MEMORY
+	else:
+		return !HAS_TRAPPED_DOOR_MEMORY
+
+func _pickup_memory_core(name: String):
+	if name == NAME_EYE_MEMORY:
+		HAS_EYE_MEMORY = true
+	elif name == NAME_HEART_MEMORY:
+		HAS_HEART_MEMORY = true
+	elif name == NAME_STATUE_MEMORY:
+		HAS_STATUE_MEMORY = true
+	elif name == NAME_TRAPPED_DOOR_MEMORY:
+		HAS_TRAPPED_DOOR_MEMORY = true
+	memory_core_count += 1
 
 func _move_to_next_day():
 	_switch_scene("res://Scenes/black_scene.tscn")
