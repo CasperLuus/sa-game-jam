b7bf3a621603fe54f7e1637459fa09be51095857
Morgan <morgan@tranzact.co.za>
2024-10-21 16:57:09 +0200

    Updated bug to get eaten
    wah

--- Scripts/bug_2.gd
+++ Scripts/bug_2.gd
@@ -7,6 +7,7 @@ var direction = 1
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var can_eat = false;
 
 func _process(delta):
 	if ray_cast_right.is_colliding():
@@ -16,3 +17,16 @@ func _process(delta):
 			direction = 1
 			animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
+	
+	if can_eat and Input.is_action_just_pressed("sleep") and GameManager.food_count < 3:
+		GameManager.food_count += 1
+		free()
+
+func _on_pickup_body_entered(body: Node2D) -> void:
+	if (body.name == "Player"):
+		can_eat = true
+
+
+func _on_pickup_body_exited(body: Node2D) -> void:
+	if (body.name == "Player"):
+		can_eat = false
