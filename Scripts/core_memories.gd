05ab7ed4e23f1b13247ab742b0e6e7a03b964f49
CASPERPC\Ghost <caspereally@gmail.com>
2024-10-21 14:19:19 +0200

    core memory progress

--- Scripts/core_memories.gd
+++ Scripts/core_memories.gd
@@ -1,10 +1,8 @@
 extends Area2D
 @onready var shiny_beacon: AudioStreamPlayer2D = $"Shiny Beacon"
-@onready var shiny_pickup: AudioStreamPlayer2D = $"Shiny Pickup"
-
-func _ready() -> void:
-	shiny_beacon.play()
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	print("My Memories are coming back to me")
-	queue_free()
+	shiny_beacon.stop()
+	animation_player.play("pickup")
