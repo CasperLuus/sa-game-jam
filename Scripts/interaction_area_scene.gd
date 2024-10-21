7f175059c5e9dd6528c8a0517946ab70ab230ade
Joshua Mitchell <18joshuam10@gmail.com>
2024-10-21 14:32:11 -0700

    interaction testing

--- Scripts/interaction_area_scene.gd
+++ Scripts/interaction_area_scene.gd
@@ -9,10 +9,7 @@ var interact: Callable = func():
 
 
 func _on_body_entered(body):
-	Interaction
-	
-
-
+	interactionmanager.register_area(self)
 
 func _on_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	InteractionArea.unregister_area(self)
